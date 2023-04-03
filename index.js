f1e39a8d66bc2a074cc70de93c12d31a453f1b68
Damon-Jo <lunaticjky@gmail.com>
2023-04-02 17:03:52 -0700

    added the register posting api and hidden security info

--- index.js
+++ index.js
@@ -2,15 +2,51 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
+
+
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:qwer1234@cluster0.rfmec.mongodb.net/?retryWrites=true&w=majority')
+// console.log(config.mongoURI)
+mongoose.connect(config.mongoURI)
 .then(()=> console.log('MongoDB Connected..'))
 .catch(err=> console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Your man')
 })
+
+app.post('/register', async (req,res)=>{
+  // when the data is loaded from client,
+  // this api put the data to database
+
+  const user = new User(req.body)
+  await user
+  .save()
+  .then(() => {
+    res.status(200).json({
+      success: true,
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    res.json({
+      success: false,
+      err: err,
+    });
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
